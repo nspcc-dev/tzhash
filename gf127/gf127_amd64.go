698a171a8068b8a6d16312298ed95d422e61b899
Roman Khimov <roman@nspcc.ru>
2023-05-17 17:26:54 +0300

    gf127: fix incorrect linter warning
    Now we've got 'unused' complaining here, but he's not the first one.
    
      var `x127x63` is unused (unused)
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- gf127/gf127_amd64.go
+++ gf127/gf127_amd64.go
@@ -11,7 +11,7 @@ package gf127
 import "golang.org/x/sys/cpu"
 
 // x127x63 represents x^127 + x^63.
-var x127x63 = GF127{msb64, msb64} //nolint:deadcode,varcheck
+var x127x63 = GF127{msb64, msb64} //nolint:deadcode,varcheck,unused
 
 // Add sets c to a+b.
 func Add(a, b, c *GF127) {
